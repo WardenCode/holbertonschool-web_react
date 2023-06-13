308ac8f7a63148a53bf8e0efa76989b3c8571a1e
Diego Linares <diegojeanluck@hotmail.com>
2023-06-13 15:35:42 -0500

    5. Merge & concat

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,15 @@
+import { List, Map } from 'immutable'
+
+export function concatElements(page1, page2) {
+    const pageList1 = List(page1);
+    const pageList2 = List(page2);
+
+    return pageList1.merge(pageList2);
+}
+
+export function mergeElements(page1, page2) {
+    const pageMap1 = Map(page1);
+    const pageMap2 = Map(page2);
+
+    return pageMap1.merge(pageMap2);
+}
