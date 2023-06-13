41f40b4f5d4853d21d303dabc29648a170ce8493
Diego Linares <diegojeanluck@hotmail.com>
2023-06-13 16:58:21 -0500

    Fix: using eslint and using the correct exports

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,18 +1,18 @@
 import { Seq } from 'immutable';
 
 export function capFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function printBestStudents(object) {
-    const seq = Seq(object);
-    const filteredStudents = seq.filter((student) => student.score > 70);
-    const students = filteredStudents.toJS();
+  const seq = Seq(object);
+  const filteredStudents = seq.filter((student) => student.score > 70);
+  const students = filteredStudents.toJS();
 
-    for (const key in students) {
-        students[key].firstName = capFirstLetter(students[key].firstName);
-        students[key].lastName = capFirstLetter(students[key].lastName);
-    }
+  Object.keys(students).forEach((key) => {
+    students[key].firstName = capFirstLetter(students[key].firstName);
+    students[key].lastName = capFirstLetter(students[key].lastName);
+  });
 
-    console.log(students);
+  console.log(students);
 }
