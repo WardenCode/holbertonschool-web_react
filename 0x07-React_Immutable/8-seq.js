e9162e933e9b7f94aa006c5b5a35529e2bfd8988
Diego Linares <diegojeanluck@hotmail.com>
2023-06-13 17:18:00 -0500

    Fix: Using only a function, use a closure and use more methods of the library immutable

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,23 +1,19 @@
 import { Seq } from 'immutable';
 
-export function capFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+export default function printBestStudents(object) {
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-export function printBestStudents(object) {
   const seq = Seq(object);
-  const filteredStudents = seq.filter((student) => {
-    student.firstName.charAt(0).toUpperCase();
-    return student.score > 70;
-  });
+  const bestStudents = seq.filter((student) => student.score > 70);
 
-  const students = filteredStudents.toJS();
+  const formatedBestStudents = bestStudents.map((student) => ({
+    score: student.score,
+    firstName: capFirstLetter(student.firstName),
+    lastName: capFirstLetter(student.lastName),
+  }));
 
-  Object.keys(students).map((key) => {
-    students[key].firstName = capFirstLetter(students[key].firstName);
-    students[key].lastName = capFirstLetter(students[key].lastName);
-    return students[key];
-  });
-
-  console.log(students);
+  const studentOBJ = formatedBestStudents.toObject();
+  console.log(studentOBJ);
 }
